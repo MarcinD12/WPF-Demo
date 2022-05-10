b0a30e9ce783bc58f02e3840f005c827c8cb3996
MarcinD123 <marcin.dziedzic49@microsoft.wsei.edu.pl>
2022-05-10 17:05:58 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logistics_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Vehicle car1 = new Vehicle("reeeee", 12);
+            //database.Vehicles.Add(car1);
+            //database.SaveChanges();
+
+            
+        }
+        
+        
+    }
+    public static class Dupa
+    {
+        public static void Send()
+        {
+            
+            Delivery_projectEntities database = new Delivery_projectEntities();
+            int cnt = 1;            
+            database.Vehicles.Add(new Vehicle("xd", cnt));
+            database.SaveChanges();
+            cnt++;
+        }
+    }
+}
